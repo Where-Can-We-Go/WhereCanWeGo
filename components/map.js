a7e57f1f0099021ed05708dda0047d2b1f02c480
Austin Barrows <austinm.barrows@gmail.com>
2022-11-05 16:23:08 -0400

    Display basic map and remove default Nextjs code

--- components/map.js
+++ components/map.js
@@ -1,5 +1,23 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function Map({ children }) {
-  return <div>Test data</div>;
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="h-screen"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
