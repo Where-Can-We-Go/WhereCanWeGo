cee13e8e5ecd5f28cd9d06b9d8d76e966b9f0052
FourNick <nleggiero1@gmail.com>
2022-11-05 19:56:04 -0400

    Changed margins

--- components/display.js
+++ components/display.js
@@ -2,12 +2,18 @@ import { Card, Text } from "@mantine/core";
 
 export default function Display() {
   return (
-    <Card shadow="sm" p="lg" radius="md" withBorder>
-        <Text weight={500} className="mb-2">Default Name</Text>
+    <Card shadow="sm" p="lg" radius="md" withBorder className="mb-3">
+      <Text weight={500} className="mb-2">
+        Default Name
+      </Text>
 
       <div className="flex justify-between">
-        <Text size="sm" color="dimmed">Default Address</Text>
-        <Text size="sm" color="dimmed">Org Type</Text>
+        <Text size="sm" color="dimmed">
+          Default Address
+        </Text>
+        <Text size="sm" color="dimmed">
+          Org Type
+        </Text>
       </div>
     </Card>
   );
