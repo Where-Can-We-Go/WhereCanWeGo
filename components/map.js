888a9260b015fcc497a9f964aae1661f4dc30118
Joseph Fleming <jflem1022@gmail.com>
2022-11-05 17:17:57 -0400

    Removed map inside of popup and sample div

--- components/map.js
+++ components/map.js
@@ -20,41 +20,7 @@ export default function Map({ children }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={[51.505, -0.09]} icon={icon} draggable={true}>
-        <Popup>
-          <MapContainer
-            center={[51.505, -0.09]}
-            zoom={13}
-            scrollWheelZoom={false}
-            className="h-[800px] w-[1000px]"
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]} icon={icon} draggable={true}>
-              <Popup>
-                <MapContainer
-                  center={[51.505, -0.09]}
-                  zoom={13}
-                  scrollWheelZoom={false}
-                  className="h-[800px] w-[1000px]"
-                >
-                  <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  />
-                  <Marker
-                    position={[51.505, -0.09]}
-                    icon={icon}
-                    draggable={true}
-                  >
-                    <Popup>Wow</Popup>
-                  </Marker>
-                </MapContainer>
-              </Popup>
-            </Marker>
-          </MapContainer>
-        </Popup>
+        <Popup></Popup>
       </Marker>
     </MapContainer>
   );
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <div>
       <Map></Map>
-      <div>asdasdasa</div>
     </div>
   );
 }
