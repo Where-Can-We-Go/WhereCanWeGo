c8e22e8e49d8f857f7856c2e9a5b5796a1f76afd
FourNick <nleggiero1@gmail.com>
2022-11-29 15:10:48 -0500

    Added mobile device responsive design
    Changed the design of the UI to work better on mobile

--- pages/index.js
+++ pages/index.js
@@ -37,9 +37,12 @@ export default function Home() {
 
   return (
     <div className="h-screen w-full">
-      <div className="h-full w-1/4 block float-left">
+      <div className="h-1/2 min-[900px]:h-screen w-full min-[900px]:w-3/4 block min-[900px]:float-right">
+        <Map></Map>
+      </div>
+      <div className="h-1/2 min-[900px]:h-full w-full min-[900px]:w-1/4 block min-[900px]:float-left">
         {/* search container */}
-        <div className="w-full h-1/6 p-4">
+        <div className="w-full p-4 h-1/4 max-[700px]:h-1/2">
           <div className="flex justify-center">
             <input
               className="w-5/6 placeholder:italic placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md p-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
@@ -48,29 +51,22 @@ export default function Home() {
               name="search"
             />
           </div>
-          <div className="pt-2 flex justify-center">
-            <Button.Group>
-              <Button variant="default">Filter 1</Button>
-              <Button variant="default">Filter 2</Button>
-              <Button variant="default">Filter 3</Button>
-              <Button variant="default">Filter 4</Button>
-              <Button variant="default">Filter 5</Button>
-            </Button.Group>
-          </div>
-          <div className="pt-1 flex justify-center">
-            <Button.Group>
-              <Button variant="default" onClick={getNonprofitData}>
-                *Load Data
-              </Button>
-              <Button variant="default">Filter 2</Button>
-              <Button variant="default">Filter 3</Button>
-              <Button variant="default">Filter 4</Button>
-            </Button.Group>
+          <div className="pt-2 inline-block text-center justify-center overscroll-contain">
+            <Button variant="default">Filter 1</Button>
+            <Button variant="default">Filter 2</Button>
+            <Button variant="default">Filter 3</Button>
+            <Button variant="default">Filter 4</Button>
+            <Button variant="default" onClick={getNonprofitData}>
+              *Load Data
+            </Button>
+            <Button variant="default">Filter 2</Button>
+            <Button variant="default">Filter 3</Button>
+            <Button variant="default">Filter 4</Button>
           </div>
         </div>
         {/* Info boxes container */}
-        <div className="w-full h-5/6 flex justify-center">
-          <ScrollArea type="hover" className="w-5/6 h-full">
+        <div className="w-full overflow-auto h-3/4 flex justify-center max-[715px]:h-1/2 ">
+          <ScrollArea type="hover" className="w-5/6">
             {nonprofits.map((npInfo, i) => {
               return (
                 <Display
@@ -104,9 +100,6 @@ export default function Home() {
             <Display></Display> */}
           </ScrollArea>
         </div>
-      </div>
-      <div className="h-screen w-3/4 block float-right">
-        <Map></Map>
       </div>
     </div>
   );
