4b0cc9604c0e62ac661581fc4a76b39b73214b49
FourNick <nleggiero1@gmail.com>
2022-12-04 20:56:02 -0500

    Fixed scaling issues on mobile

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,10 @@ export default function Home() {
       searchVal: "",
     },
     validate: {
-      searchVal: (value) => (value.length != 5 || /[^\d]/.test(value) ? 'Please enter a valid 5-digit zipcode' : null),
+      searchVal: (value) =>
+        value.length != 5 || /[^\d]/.test(value)
+          ? "Please enter a valid 5-digit zipcode"
+          : null,
     },
   });
 
@@ -57,7 +60,7 @@ export default function Home() {
       </div>
       <div className="h-1/2 min-[1200px]:h-full w-full min-[1200px]:w-1/3 block min-[1200px]:float-left min-[1620px]:w-1/4">
         {/* search container */}
-        <div className="w-full p-4 h-1/4 max-[1200px]:h-1/2">
+        <div className="w-full p-4 h-1/3 max-[1200px]:h-1/2">
           <div className="flex justify-center">
             <form
               onSubmit={form.onSubmit((values) => {
@@ -66,26 +69,28 @@ export default function Home() {
                 form.reset();
               })}
             >
-              <TextInput // Textbox for the website's search bar
-                className="w-full"
-                placeholder="Enter a zipcode..."
-                {...form.getInputProps("searchVal")}
-              />
-
-              <Button
-                variant="default"
-                type="submit"
-              >
-                Search
-              </Button>
+              <div className="grid grid-cols-3">
+                <div className="col-span-2">
+                  <TextInput // Textbox for the website's search bar
+                    className="w-full"
+                    placeholder="Enter a zipcode..."
+                    {...form.getInputProps("searchVal")}
+                  />
+                </div>
+                <div>
+                  <Button variant="default" type="submit" className="w-full">
+                    Search
+                  </Button>
+                </div>
+              </div>
             </form>
           </div>
           <div className="h-3/4">
-            <div className="h-5/6 text-center items-center grid grid-flow-col auto-cols-auto gap-2 justify-center">
+            <div className="h-3/4 text-center items-center grid grid-flow-col auto-cols-auto gap-2 justify-center">
               <div>
                 <Button
                   variant="default"
-                  className="rounded-full w-[75px] h-[75px] hover:border-4 hover:bg-slate-100"
+                  className="rounded-full w-[75px] h-[75px] max-[1200px]:w-[50px] max-[1200px]:h-[50px] hover:border-4 hover:bg-slate-100"
                 >
                   <img src="\images\034-house.png" className="w-[50px]"></img>
                 </Button>
@@ -94,7 +99,7 @@ export default function Home() {
               <div>
                 <Button
                   variant="default"
-                  className="rounded-full w-[75px] h-[75px] hover:border-4 hover:bg-slate-100"
+                  className="rounded-full w-[75px] h-[75px] max-[1200px]:w-[50px] max-[1200px]:h-[50px] hover:border-4 hover:bg-slate-100"
                 >
                   <img src="\images\030-food.png" className="w-[50px]"></img>
                 </Button>
@@ -103,7 +108,7 @@ export default function Home() {
               <div>
                 <Button
                   variant="default"
-                  className="rounded-full w-[75px] h-[75px] hover:border-4 hover:bg-slate-100"
+                  className="rounded-full w-[75px] h-[75px] max-[1200px]:w-[50px] max-[1200px]:h-[50px] hover:border-4 hover:bg-slate-100"
                 >
                   <img
                     src="\images\021-teamwork.png"
@@ -115,7 +120,7 @@ export default function Home() {
               <div>
                 <Button
                   variant="default"
-                  className="rounded-full w-[75px] h-[75px] hover:border-4 hover:bg-slate-100"
+                  className="rounded-full w-[75px] h-[75px] max-[1200px]:w-[50px] max-[1200px]:h-[50px] hover:border-4 hover:bg-slate-100"
                 >
                   <img
                     src="\images\018-stethoscope.png"
@@ -125,7 +130,7 @@ export default function Home() {
                 <p className="center text-sm">Health</p>
               </div>
             </div>
-            <div className="h-1/5 text-center">
+            <div className="h-1/4 text-center">
               <Button
                 variant="default"
                 className="rounded-full hover:bg-slate-100"
@@ -136,7 +141,7 @@ export default function Home() {
           </div>
         </div>
         {/* Info boxes container */}
-        <div className="w-full overflow-auto h-3/4 flex justify-center max-[1200px]:h-1/2 ">
+        <div className="w-full overflow-auto h-2/3 flex justify-center max-[1200px]:h-1/2 ">
           <ScrollArea type="hover" className="w-5/6">
             {nonprofits.map((npInfo, i) => {
               return (
