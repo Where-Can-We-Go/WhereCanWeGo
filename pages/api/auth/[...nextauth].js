d3f3414aad853074922c37e496b47fc324ada5f2
Austin Barrows <austinm.barrows@gmail.com>
2022-12-06 16:28:07 -0500

    Remove unneeded code; apply Prettier formatting

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -7,53 +7,26 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
 
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-  
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null
-  
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      }
-    }),
-    // ...add more providers here
     NextAuth({
-        callbacks: {
-            async jwt({ token, account }) {
-              // Persist the OAuth access_token to the token right after signin
-              if (account) {
-                token.accessToken = account.access_token
-              }
-              return token
-            },
-            async session({ session, token, user }) {
-              // Send properties to the client, like an access_token from a provider.
-              session.accessToken = token.accessToken
-              return session
-            }
-        }
+      callbacks: {
+        async jwt({ token, account }) {
+          // Persist the OAuth access_token to the token right after signin
+          if (account) {
+            token.accessToken = account.access_token;
+          }
+          return token;
+        },
+        async session({ session, token, user }) {
+          // Send properties to the client, like an access_token from a provider.
+          session.accessToken = token.accessToken;
+          return session;
+        },
+      },
     }),
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
